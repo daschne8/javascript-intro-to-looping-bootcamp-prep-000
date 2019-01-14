c4fac3e979675096146a518851edf92fcfd54517
Daniel Schneider <daschne8@gmail.com>
2019-01-14 03:08:13 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+function doWhileLoop(array){
+  for(var i = array.length;i>0;i--){
+    array.pop();
+  }
+  return array;
+}
+function whileLoop(n){
+  while (n >= 0) {
+    n--;
+  }
+  return 'done';
+}
+function forLoop(){
+  var array = [];
+  for(var i=0;i<25;i++){
+    //array[i] = "test";
+    console.log(i);
+    //array[i] = "I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.";
+
+  }
+}
