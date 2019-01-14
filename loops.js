809926b81cf4ae0b0a165ad2f3949ec6e120c258
Daniel Schneider <daschne8@gmail.com>
2019-01-14 03:19:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,9 +15,9 @@ function forLoop(){
   for(var i=0;i<25;i++){
     //console.log(i);
     if(i === 1){
-      array[i] = "I am 1 strange loop.";
+      array.push("I am 1 strange loop.");
     }else{
-      array[i] = "I am ${i} strange loops";
+      array.push("I am ${i} strange loops");
     }
     //array[i] = "I am ${i} strange loop${i === 0 ? \'\' : \'s\'}.";
       console.console.log(array[i]);
